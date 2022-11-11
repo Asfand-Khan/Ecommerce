fe3b1f5edab58fc63b903c104fcb01e5316a22d2
ASFAND SHAMIM <khanasfand174@gmail.com>
2022-11-12 01:35:25 +0500

    added custom error handler

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -9,8 +9,21 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // duplicate key error
   if (err.code === "E11000") {
     const message = `duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // json web token error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Token is invalid try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Json web token expire error
+  if (err.name === "JsonWebExpireError") {
+    const message = `Token is expired try again`;
     err = new ErrorHandler(message, 400);
   }
 
